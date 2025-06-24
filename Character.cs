5a6eb81dcb59649e84c8eb25e88d338efddd4858
Trenton Scott <InsaneGriot@Outlook.com>
2025-06-24 08:55:32 -0400

    Initial

---
+++ Character.cs
@@ -0,0 +1,11 @@
+﻿namespace StarReverieCore
+{
+    public class Character
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int Level { get; set; }
+        public int PowerLevel { get; set; }
+    }
+}
