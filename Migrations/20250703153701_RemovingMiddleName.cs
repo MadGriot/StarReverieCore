63aa8b9531703bad41ec71e8fe77eff99627e352
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-03 11:38:36 -0400

    Removing Middle Name

---
+++ Migrations/20250703153701_RemovingMiddleName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StarReverieCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovingMiddleName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MiddleName",
+                table: "Characters");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MiddleName",
+                table: "Characters",
+                type: "TEXT",
+                nullable: true);
+        }
+    }
+}
--- Models/Character.cs
+++ Models/Character.cs
@@ -4,7 +4,6 @@
     {
         public int Id { get; set; }
         public required string FirstName { get; set; }
-        public string? MiddleName { get; set; }
         public required string LastName { get; set; }
         public int Level { get; set; }
         public int PowerLevel { get; set; }
