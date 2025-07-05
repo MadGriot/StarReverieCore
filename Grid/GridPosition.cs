7e8eb4c259f8c6303483a33101858f762812e65a
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-04 23:24:55 -0400

    GridPosition

---
+++ Grid/GridPosition.cs
@@ -0,0 +1,40 @@
+﻿
+namespace StarReverieCore.Grid
+{
+    public class GridPosition : IEquatable<GridPosition>
+    {
+        public int x;
+        public int y;
+        public int z;
+
+        public GridPosition(int x, int y, int z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is GridPosition position &&
+                x == position.x &&
+                y == position.y &&
+                z == position.z;
+        }
+        public bool Equals(GridPosition other) => this == other;
+        public override int GetHashCode() => HashCode.Combine(x, y, z);
+
+        public override string ToString() => $"x {x}; z: {z}";
+
+        public static bool operator ==(GridPosition a, GridPosition b) =>
+            a.x == b.x && a.y == b.y && a.z == b.z;
+
+        public static bool operator !=(GridPosition a, GridPosition b) => !(a == b);
+
+        public static GridPosition operator +(GridPosition a, GridPosition b) =>
+            new GridPosition(a.x + b.x, a.y + b.y, b.z + b.z);
+
+        public static GridPosition operator -(GridPosition a, GridPosition b) =>
+            new GridPosition(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+}
