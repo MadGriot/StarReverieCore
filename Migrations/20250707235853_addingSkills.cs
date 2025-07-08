09e65a66c1db51465b567a4a526dfdf331379f43
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-07 20:00:01 -0400

    EFCore Skills

---
+++ Migrations/20250707235853_addingSkills.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StarReverieCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class addingSkills : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Skills",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Level = table.Column<int>(type: "INTEGER", nullable: false),
+                    SkillDifficulty = table.Column<int>(type: "INTEGER", nullable: false),
+                    Skill = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Skills", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Skills");
+        }
+    }
+}
