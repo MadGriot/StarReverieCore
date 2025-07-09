40670943fe1aa71a8e328db3708c10c682d64a3e
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-08 23:54:15 -0400

    Skill Cost Calculator

---
+++ Mechanics/SkillCostCalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarReverieCore.Mechanics
+{
+    public static class SkillCostCalculator
+    {
+        public static int GetSkillCost(SkillDifficulty difficulty, int level)
+        {
+            switch (difficulty)
+            {
+                case SkillDifficulty.Easy:
+
+                    return(1 * level);
+                case SkillDifficulty.Average:
+                    break;
+                case SkillDifficulty.Hard:
+                    break;
+                case SkillDifficulty.VeryHard:
+                    break;
+            }
+            return 0;
+        }
+    }
+}
