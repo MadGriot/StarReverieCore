b4a3a8798b854e457a508def8ec8af444e4f88b5
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-11 16:29:08 -0400

    Adding more migrations

---
+++ Migrations/20250711202835_SkillMapping.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StarReverieCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class SkillMapping : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Skills_Characters_CharacterId1",
+                table: "Skills");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Skills_CharacterId1",
+                table: "Skills");
+
+            migrationBuilder.DropColumn(
+                name: "CharacterId1",
+                table: "Skills");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CharacterId1",
+                table: "Skills",
+                type: "INTEGER",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Skills_CharacterId1",
+                table: "Skills",
+                column: "CharacterId1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Skills_Characters_CharacterId1",
+                table: "Skills",
+                column: "CharacterId1",
+                principalTable: "Characters",
+                principalColumn: "Id");
+        }
+    }
+}
