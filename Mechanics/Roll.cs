ae1fac55e61919a89d43163beca27ac8a5e3ecd7
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-11 23:40:13 -0400

    Basic Dice Roll Mechanics

---
+++ Mechanics/Roll.cs
@@ -0,0 +1,15 @@
+﻿namespace StarReverieCore.Mechanics
+{
+    public class Roll
+    {
+        private static Random rnd = new Random();
+
+        public static int _3d6() => rnd.Next(1, 7) + rnd.Next(1, 7) + rnd.Next(1, 7);
+
+        public static int _2d6() => rnd.Next(1, 7) + rnd.Next(1, 7);
+          
+
+        public static int _1d6() => rnd.Next(1, 4);
+        
+    }
+}
