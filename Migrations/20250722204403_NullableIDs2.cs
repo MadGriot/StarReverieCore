083ccef1872a6104f9ae9af5a9f93949fec37928
Trenton Scott <InsaneGriot@Outlook.com>
2025-07-22 16:49:45 -0400

    Nullable Ids

---
+++ Migrations/20250722204403_NullableIDs2.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace StarReverieCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class NullableIDs2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weapons_Characters_CharacterId",
+                table: "Weapons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weapons_Skills_SkillId",
+                table: "Weapons");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SkillId",
+                table: "Weapons",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CharacterId",
+                table: "Weapons",
+                type: "INTEGER",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weapons_Characters_CharacterId",
+                table: "Weapons",
+                column: "CharacterId",
+                principalTable: "Characters",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weapons_Skills_SkillId",
+                table: "Weapons",
+                column: "SkillId",
+                principalTable: "Skills",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weapons_Characters_CharacterId",
+                table: "Weapons");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weapons_Skills_SkillId",
+                table: "Weapons");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "SkillId",
+                table: "Weapons",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CharacterId",
+                table: "Weapons",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "INTEGER",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weapons_Characters_CharacterId",
+                table: "Weapons",
+                column: "CharacterId",
+                principalTable: "Characters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weapons_Skills_SkillId",
+                table: "Weapons",
+                column: "SkillId",
+                principalTable: "Skills",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- Models/Unit.cs
+++ Models/Unit.cs
@@ -5,9 +5,9 @@ namespace StarReverieCore.Models
     {
         public int Id { get; set; }
         public required string Name { get; set; }
-        public int CharacterId { get; set; }
-        public Character Character { get; set; }
-        public int SkillId { get; set; }
+        public int? CharacterId { get; set; }
+        public Character? Character { get; set; }
+        public int? SkillId { get; set; }
         public SkillModel? SkillModel { get; set; }
     }
 }
